6d61deef32f39da57c454171e45cc1a3bc4f0812
Vít Bednář <vit.bednar@assecosol.com>
2019-03-26 11:02:27 +0100

    Přidejte soubory projektu.

---
+++ Catcher/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Catcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(770, 329);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(649, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(672, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Catcher/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Catcher
+{
+    public partial class Form1 : Form
+    {
+        static public System.Collections.Generic.List<string> list = new List<string>();
+        Watcher watcher;
+        Timer tick;
+        int lastlistrow = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Vybrat složku";
+            tick = new Timer();
+            tick.Interval = 1;
+            tick.Tick += Tick_Tick;
+            tick.Start();
+            System.IO.Directory.CreateDirectory("copyed");
+        }
+
+        private void Tick_Tick(object sender, EventArgs e)
+        {
+            if (lastlistrow < list.Count)
+            {
+                for (int i = lastlistrow +1 ; i < list.Count; i++)
+                {
+                    listBox1.Items.Add(list[i]);
+                }
+                lastlistrow = list.Count;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+            watcher = new Watcher(folderBrowserDialog1.SelectedPath, listBox1);
+            
+        }
+        public static string Now()
+        {
+            return DateTime.Now.ToString("yyyyMMddHHmmssFFFFFFF");
+            
+        }
+    }
+    public class Watcher
+    {
+        private string folder;
+        private ListBox listBox;
+
+        public string Folder { get => folder; set => folder = value; }
+
+        public Watcher(string folder, ListBox listBox)
+        {
+            this.folder = folder;
+            this.listBox = listBox;
+            
+            System.IO.FileSystemWatcher watcher = new System.IO.FileSystemWatcher(folder);
+            watcher.Deleted += (s,e) => Form1.list.Add(Form1.Now() + "DELETED " + e.Name);
+            watcher.Changed += Watcher_Changed;
+            watcher.Error += (s, e) => Form1.list.Add(Form1.Now() + "MONITORING STOPPED");
+            watcher.Created += Watcher_Created;
+
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void Watcher_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            try
+            {
+                if (e.Name.Contains("\\"))
+                {
+                    int notfile = e.Name.LastIndexOf('\\');
+                    System.IO.Directory.CreateDirectory("copyed\\" + e.Name.Substring(0, notfile));
+
+                }
+                System.IO.File.Copy(e.FullPath, "copyed\\" + e.Name + Form1.Now());
+                Form1.list.Add(Form1.Now() + "CHANGED " + e.Name);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Watcher_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            try
+            {
+                if (e.Name.Contains("\\"))
+                {
+                    int notfile = e.Name.LastIndexOf('\\');
+                        System.IO.Directory.CreateDirectory("copyed\\"+e.Name.Substring(0, notfile));
+                    
+                }
+                System.IO.File.Copy(e.FullPath, "copyed\\" + e.Name + Form1.Now());
+                Form1.list.Add(Form1.Now() + "CREATED " + e.Name);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
